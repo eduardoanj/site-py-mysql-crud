a04c354f2bbf1758bc73d55967d8538954accb46
Eduardo <eduardo_anjos15@yahoo.com>
2019-10-27 18:55:22 -0300

    problemas

--- rotas.py
+++ rotas.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicial():
-    return ('olaaaaar pessoas')
+    return render_template('first_tela.html')
+
+@app.route('/index')
+def index():
+    return render_template ('index.html')
 
 
 
